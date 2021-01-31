f9d4174cb7b988badc4d0e59bfef647044b14add
Yakov Markovitch <yakov.spare@gmail.com>
2021-01-31 04:21:23 +0300

    Cleanup createPackage.sh

--- setup/Linux/createPackages.sh
+++ setup/Linux/createPackages.sh
@@ -1,78 +1,64 @@
 #!/bin/sh
+# Exit on error
+set -e
+
+SRCDIR="$(readlink -f "$(dirname "$0")/../..")"
 
 # index sample projects
+SOURCETRAIL="$SRCDIR/build/app/Sourcetrail"
+PROJECTS_PATH="$SRCDIR/bin/app/user/projects"
 
-BIN_PATH=build/Release/app/Sourcetrail
-PROJECTS_PATH=bin/app/user/projects
-
-./$BIN_PATH index --full $PROJECTS_PATH/tictactoe_cpp/tictactoe_cpp.srctrlprj
-./$BIN_PATH index --full $PROJECTS_PATH/tictactoe_py/tictactoe_py.srctrlprj
-./$BIN_PATH index --full $PROJECTS_PATH/tutorial/tutorial.srctrlprj
-./$BIN_PATH index --full $PROJECTS_PATH/javaparser/javaparser.srctrlprj
-
+for project in tictactoe_cpp tictactoe_py tutorial javaparser
+do
+    "$SOURCETRAIL" index --full "$PROJECTS_PATH/$project/$project.srctrlprj"
+done
 
 # create AppDir
-
 rm -rf AppDir
 
-mkdir AppDir
-cd AppDir
+PREFIX="$SRCDIR/AppDir/Sourcetrail"
 
-mkdir usr
-cd usr
+mkdir -p "$PREFIX/"{bin,lib,share}
 
-mkdir bin
-mkdir lib
-mkdir share
+cp -T "$SRCDIR/build/app/Sourcetrail" "$PREFIX/bin/sourcetrail"
+cp    "$SRCDIR/build/app/sourcetrail_indexer" "$PREFIX/bin/"
 
-cd bin
-cp ../../../build/Release/app/Sourcetrail sourcetrail
-cp ../../../build/Release/app/sourcetrail_indexer .
-cd ..
+pushd "$PREFIX/share"
+# Create share/applications, share/data, share/mime/packages
+mkdir -p applications data mime/packages
 
-cd share
+cp "$SRCDIR/setup/Linux/data/sourcetrail.desktop"  applications
+cp "$SRCDIR/setup/Linux/data/sourcetrail-mime.xml" mime/packages
 
-mkdir applications
-cd applications
-cp ../../../../setup/Linux/data/sourcetrail.desktop .
-cd ..
+for datadir in \
+    color_schemes \
+    syntax_highlighting_rules \
+    fonts \
+    gui \
+    java \
+    python \
+    fallback \
+    license \
+    cxx
+do
+    cp -R -t data "$SRCDIR/bin/app/data/$datadir"
+done
+cp -R -t data/fallback "$SRCDIR/bin/app/user/projects"
 
-mkdir mime
-cd mime
-mkdir packages
-cd packages
-cp ../../../../../setup/Linux/data/sourcetrail-mime.xml .
-cd ../..
-
-mkdir data
-cd data
-cp -R ../../../../bin/app/data/color_schemes .
-cp -R ../../../../bin/app/data/syntax_highlighting_rules .
-cp -R ../../../../bin/app/data/fonts .
-cp -R ../../../../bin/app/data/gui .
-cp -R ../../../../bin/app/data/java .
-cp -R ../../../../bin/app/data/python .
-cp -R ../../../../bin/app/data/fallback .
-cp -R ../../../../bin/app/user/projects fallback/
-cp -R ../../../../bin/app/data/license .
-cp -R ../../../../bin/app/data/cxx .
-cd ..
-
-cd ../../..
+popd # $PREFIX/share
 
 # print file structure
 # find AppDir | sed -e "s/[^-][^\/]*\// |/g" -e "s/|\([^ ]\)/|-\1/"
 
-
 # create icons
 
-ICON=AppDir/usr/share/data/gui/icon/logo_1024_1024.png
-HICOLOR_DIR=AppDir/usr/share/icons/hicolor
+ICON="$PREFIX/share/data/gui/icon/logo_1024_1024.png"
+HICOLOR_DIR="$PREFIX/share/icons/hicolor"
 
 createIcon()
 {
-    mkdir -p $HICOLOR_DIR/$1/apps/
-    convert $ICON -resize $1 $HICOLOR_DIR/$1/apps/sourcetrail.png
+    mkdir -p "$HICOLOR_DIR/$1/apps/"
+    convert "$ICON" -resize $1 "$HICOLOR_DIR/$1/apps/sourcetrail.png"
 }
 
 createIcon 512x512
@@ -81,36 +67,4 @@ createIcon 128x128
 createIcon 64x64
 createIcon 48x48
 
-
-# Version for filename
-
-VERSION=$(cat build/Release/version.txt)
-VERSION=${VERSION/./_}
-VERSION=${VERSION/./_}
-export VERSION
-
-
-# create .AppImage
-
-linuxdeployqt AppDir/usr/bin/sourcetrail_indexer -qmake=$Qt5_DIR/bin/qmake -ignore-glob=*python*
-rm -f AppDir/AppRun
-
-linuxdeployqt AppDir/usr/share/applications/sourcetrail.desktop -qmake=$Qt5_DIR/bin/qmake -ignore-glob=*python* -appimage
-
-rename x86_64 Linux_64bit *.AppImage
-rename - _ *.AppImage
-rename - _ *.AppImage
-
-
-# create .tar.gz
-
-mv AppDir/usr/ Sourcetrail
-cp -R setup/Linux/data/package/* Sourcetrail
-
-tar -czvf Sourcetrail_${VERSION}_Linux_64bit.tar.gz Sourcetrail
-
-
-# cleanup
-
-rm -rf AppDir
-rm -rf Sourcetrail
+cp -R -t "$PREFIX" "$SRCDIR/setup/Linux/data/package"/*
