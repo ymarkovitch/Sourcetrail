ac8b7f6eedf14e613cffa632b5663aeac88ef5d5
Yakov Markovitch <yakov.spare@gmail.com>
2021-01-31 04:25:02 +0300

    Drop setting  of LD_LIBRARY_PATH from Sourcetrail launch script

--- setup/Linux/data/package/Sourcetrail.sh
+++ setup/Linux/data/package/Sourcetrail.sh
@@ -3,7 +3,6 @@
 # Get the current directory in canonical form (resolve all the symlinks, if present)
 SOURCETRAIL_PATH="$(dirname "$(readlink -f "$0")")"
 
-export LD_LIBRARY_PATH="$SOURCETRAIL_PATH/lib:$LD_LIBRARY_PATH"
 export QT_XKB_CONFIG_ROOT="/usr/share/X11/xkb:$QT_XKB_CONFIG_ROOT"
 export QT_QPA_PLATFORM_PLUGIN_PATH="$SOURCETRAIL_PATH/plugins/platforms"
 export QT_QPA_FONTDIR="$SOURCETRAIL_PATH/data/fonts:$QT_QPA_FONTDIR"
